e308c5e86eca3fc3487d36ec86b4858512d38ffb
asimkoul <ahmadasim080gmail.com>
2024-04-22 22:43:50 +0530

    addingControllers

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,17 @@ const express=require('express')
 const router=express.Router()
 const path=require('path')
 const rootDir=require('../util/path')
-
+const contact=require('../controllers/contactUs')
+const postSucc=require('../controllers/success')
 router.get('/add-product',(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','add-product.html'))
 })
-router.get('/contactus' , (req,res,next) => {
-    res.sendFile(path.join(rootDir,'views' ,'contact-us.html'))
-});
+router.get('/contactus',contact.getContact);
 
 router.post('/add-product',(req,res,next)=>{
     console.log(req.body)
     res.redirect('/')
 })
-router.post('/success' , (req,res,next) => {
-    res.sendFile(path.join(rootDir,'views' ,'success.html'))})
+router.post('/success',postSucc.getSuccess)
 
 module.exports=router
